7d66f9a820d0b3a57afa4388eac0a837f7dad429
KAreEMN09705 <flicker.02.vaults@icloud.com>
2025-02-13 02:30:10 -0600

    合并了 feature-branch 分支

---
+++ autonomic-administration-algorithms/src/main/java/br/com/autonomiccs/autonomic/administration/algorithms/beans/ClusterVmProfile.java
@@ -0,0 +1,82 @@
+/*
+ * This program is part of Autonomiccs "autonomic-platform",
+ * an open source autonomic cloud computing management platform.
+ * Copyright (C) 2016 Autonomiccs, Inc.
+ *
+ * Licensed to the Autonomiccs, Inc. under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The Autonomiccs, Inc. licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package br.com.autonomiccs.autonomic.administration.algorithms.beans;
+
+/**
+ * Contains the cluster VMs profile attributes (number of instances, total cpus,
+ * total cpu speed, memory, cpus profile, cpus speed profile, memory profile).
+ */
+public class ClusterVmProfile {
+
+    private int numberOfInstances;
+    private int totalCpus;
+    private int totalCpuSpeed;
+    private int totalMemory;
+    private double cpusProfile;
+    private double cpuSpeedProfile;
+    private double memoryProfile;
+
+    public int getNumberOfInstances() {
+        return numberOfInstances;
+    }
+    public void setNumberOfInstances(int totalInstances) {
+        this.numberOfInstances = totalInstances;
+    }
+    public int getTotalCpus() {
+        return totalCpus;
+    }
+    public void setTotalCpus(int totalCpus) {
+        this.totalCpus = totalCpus;
+    }
+    public int getTotalCpuSpeed() {
+        return totalCpuSpeed;
+    }
+    public void setTotalCpuSpeed(int totalCpuSpeed) {
+        this.totalCpuSpeed = totalCpuSpeed;
+    }
+    public int getTotalMemory() {
+        return totalMemory;
+    }
+    public void setTotalMemory(int totalMemory) {
+        this.totalMemory = totalMemory;
+    }
+    public double getCpusProfile() {
+        return cpusProfile;
+    }
+    public void setCpusProfile(double cpusProfile) {
+        this.cpusProfile = cpusProfile;
+    }
+    public double getCpuSpeedProfile() {
+        return cpuSpeedProfile;
+    }
+    public void setCpuSpeedProfile(double speedProfile) {
+        this.cpuSpeedProfile = speedProfile;
+    }
+    public double getMemoryProfile() {
+        return memoryProfile;
+    }
+    public void setMemoryProfile(double memoryProfile) {
+        this.memoryProfile = memoryProfile;
+    }
+
+}
